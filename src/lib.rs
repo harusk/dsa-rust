829cf042b4c99a66f23e8ad290c34500ac813333
vz000 <valeriazursa@gmail.com>
2026-07-11 22:01:10 -0600

    Add merge sort. In this stage, it is incomplete.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 mod insertion_sort;
+mod merge_sort;
+
 pub use insertion_sort::insertion_sort::*;
+pub use merge_sort::merge_sort::*;
 
 #[cfg(test)]
 mod tests {
@@ -18,6 +21,14 @@ mod tests {
         let mut arr = [1.2, 3.45, 23.4, 10.2, 32.0];
         let arr_result = [1.2, 3.45, 10.2, 23.4, 32.0];
         sort(&mut arr, 5);
+        assert_eq!(arr, arr_result);
+    }
+
+    #[test]
+    fn sort_with_merge_sort() {
+        let mut arr = vec![12, 3, 7, 9, 14, 6, 11, 2];
+        let arr_result = vec![2, 3, 6, 7, 9, 11, 12, 14];
+        merge_sort(&mut arr, 0, 8);
         assert_eq!(arr, arr_result);
     }
 }
